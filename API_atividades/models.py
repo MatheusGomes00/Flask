c45d782611620e43520c9a6c9b29f51c002a8e29
MatheusGomes notebook <matheus.recin@outlook.com>
2023-10-23 15:54:37 -0300

    Segunda_23-10-23_15:54

--- API_atividades/models.py
+++ API_atividades/models.py
@@ -1,33 +1,43 @@
-# classes que vão referenciar as tabelas do banco de dados
+# Este código em Python é um exemplo de como definir e usar classes que mapeiam tabelas em um banco de dados SQLite usando o SQLAlchemy.
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker, relationship
 
-# O engine é para ser apenas a ponte de conexão entre o Python e o banco.
+# Cria uma conexão com o banco de dados SQLite
 engine = create_engine('sqlite:///atividades.db')  # create_engine(mydb:/..., echo=True) --> registra no console todos os comandos SQL executados, !!não usar em produção!!
+
+""" Cria uma sessão com o banco de dados
+A sessão é uma interface para executar operações no banco de dados. 
+A opção autocommit=False significa que as transações não são confirmadas 
+automaticamente; você precisa confirmá-las manualmente com db_session.commit()."""
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
+# Cria uma classe base para definição das tabelas
 Base = declarative_base()
+# Associa a sessão de banco de dados à classe base para que ela possa ser usada posteriormente para consultar o banco de dados.
 Base.query = db_session.query_property()
 
 
+# Define a classe "Pessoas" que mapeia a tabela 'Pessoas' no banco de dados
 class Pessoas(Base):
     __tablename__ = 'pessoas'
     id = Column(Integer, primary_key=True)
     nome = Column(String(40), index=True)
     idade = Column(Integer)
 
-    def __repr__(self):  # forma de representar o objeto ao fazer chamadas
+    # Métodos para representar e manipular objetos
+    def __repr__(self):  # representação
         return '<Pessoa {}>'.format(self.nome)
 
-    def save(self):
+    def save(self):  # manipulação
         db_session.add(self)
         db_session.commit()
 
-    def delete(self):
+    def delete(self):  # manipulação
         db_session.delete(self)
         db_session.commit()
 
 
+# Define a classe Atividades que mapeia a tabela 'atividades' no banco de dados
 class Atividades(Base):
     __tablename__ = 'atividades'
     id = Column(Integer, primary_key=True)
@@ -35,20 +45,21 @@ class Atividades(Base):
     pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     pessoa = relationship("Pessoas")
 
-    def __repr__(self):  # forma de representar o objeto ao fazer chamadas
+    # Métodos para representar e manipular objetos
+    def __repr__(self):  # representação
         return '<Pessoa {}>'.format(self.nome)
 
-    def save(self):
+    def save(self):  # manipulação
         db_session.add(self)
         db_session.commit()
 
-    def delete(self):
+    def delete(self):  # manipulação
         db_session.delete(self)
         db_session.commit()
 
 
 def init_db():
-    Base.metadata.create_all(bind=engine)  # cria o banco de dados
+    Base.metadata.create_all(bind=engine)  # cria as tabelas do banco de dados
 
 
 if __name__ == '__main__':
